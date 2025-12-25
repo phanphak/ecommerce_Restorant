c2964aa05b823dd19038d2b8dc8e69ba66791b7e
Phan Phak <phanphak639@gmail.com>
2025-12-25 08:43:54 -0800

    fill dotenv

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("phan phak");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+// dotenv configuration
+dotenv.config();
+
+
+
+// rest object
+app.use(express.json());
+
+
+//Mindoware
+app.use("/api", (req, res)=>{
+   res.status(200).json({message: "Respone pages!"})
+})
+
+
+
+
+// port server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (req, res)=>{
+    console.log(`server running PORT ${PORT}`);
+})
+
